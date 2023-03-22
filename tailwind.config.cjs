15500dcf068a723f40ad362759a9f21ab482e9e3
Riku Laajala <riku.laajala@gmail.com>
2023-03-22 23:02:05 +0200

    remove unused themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+    theme: {
+        extend: {}
+    },
 
-  plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ['dark', 'light'],
+    }
 };
 
 module.exports = config;
